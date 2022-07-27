7aa1350f70b9fc44f4989769b2e12206fdee5622
Josue Mpia <85482375+JosueMpia@users.noreply.github.com>
2022-07-27 06:31:59 -0400

    Updated Reward Points
    Team, today during the meeting, let take a look at this one that I updated. Once the keeper is working on this one, I can use the same formula on other contract.

--- rewardPoint.sol
+++ rewardPoint.sol
@@ -1,39 +1,79 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8;
+
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
-interface RewardPoint {
-    function mint(address account, uint256 amount) external returns (bool);
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
 }
 
+contract BetterticketReward is KeeperCompatibleInterface{
+    IERC20 public rewardsToken;
+    address public owner;
 
-// Minimum duration of each round of rewards in seconds is 86400
+    // Duration of rewards to be paid out every week, Owner will need to setup new reward duration when the last one ends depending on the users demands
+    //  --- Everything (in seconds)
+    uint public duration;
 
-contract BetterticketPoint is KeeperCompatibleInterface{
+    // Timestamp of when the rewards finish - December 30th, 2022
+    uint public finishAt;
 
-    RewardPoint public minter;
+    // User address => rewards claimed
+    mapping(address => uint) public rewards;
+
+     // Rate of the reward is 10 token per duration the owner set
+     // Owner can also reduce the amount of the reward depedning on the users demands --- Everything (in seconds)
+    uint256 rewardRate = 10; 
+  
     uint public immutable interval;
     uint public lastTimeStamp;
-    uint256 reward = 10;
-    uint256 rewardInWei = 0;  
 
-constructor (uint updateInterval) {
+     modifier onlyOwner() {
+        require(msg.sender == owner, "not authorized");
+        _;
+    }
+
+    constructor (uint updateInterval) {
     interval = updateInterval;
     lastTimeStamp = block.timestamp;
-    minter = RewardPoint(0x20Ec195ba5C43193A726cEC44067d791f97cB6dE);
+    rewardsToken = IERC20(0x20Ec195ba5C43193A726cEC44067d791f97cB6dE);
 }
 
+    function lastTimeRewardApplicable() public view returns (uint) {
+        return _min(finishAt, block.timestamp);
+    }
 
-function checkUpkeep(bytes calldata  checkData ) external view override returns (bool upkeepNeeded, bytes memory performData ) {
+    function setRewardsDuration(uint _duration) external onlyOwner {
+        require(finishAt < block.timestamp, "reward duration not finished");
+        duration = _duration;
+    }
+
+    function _min(uint x, uint y) private pure returns (uint) {
+        return x <= y ? x : y;
+    }
+
+    function checkUpkeep(bytes calldata  checkData ) external view override returns (bool upkeepNeeded, bytes memory performData ) {
     upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
         performData = checkData;
     }
 
     function performUpkeep(bytes calldata performData) external override {
-           {                
-                rewardInWei = reward * 10 ** 18;
-                minter.mint(msg.sender, rewardInWei);
-           }
+       rewardsToken.transfer(msg.sender, rewardRate);
        lastTimeStamp = block.timestamp;
        performData;
     }
